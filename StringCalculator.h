e83091cc378ed01be23670c3d820a1110b9e2eb4
RoopashreeHT <BHAL1KOR@bosch.com>
2024-09-13 18:13:12 +0530

    refcator the code to meet ccn for 3 test cases

--- StringCalculator.h
+++ StringCalculator.h
@@ -2,12 +2,26 @@
 #include <stdlib.h>
 #include <string.h>
 
-int add(const char* numbers)
-{
+#define CONDITION_NOT_MEET 1
+
+int HandleEmptyOrNullInput(const char* numbers) {
     if (numbers == NULL || strlen(numbers) == 0) {
         return 0;
     }
+    return CONDITION_NOT_MEET; 
+}
+
+int HandleSingleZeroInput(const char* numbers) {
     if (strcmp(numbers, "0") == 0) {
+        return 0;
+    }
+    return CONDITION_NOT_MEET; 
+}
+
+int add(const char* numbers)
+{
+    if(HandleEmptyOrNullInput(numbers) * HandleSingleZeroInput(number))
+    {
         return 0;
     }
 
