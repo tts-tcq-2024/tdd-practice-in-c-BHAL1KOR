aabdd87e019684efc5bb414ee6c5462e573255fd
RoopashreeHT <BHAL1KOR@bosch.com>
2024-09-13 19:01:29 +0530

    refcatoring

--- StringCalculator.h
+++ StringCalculator.h
@@ -5,57 +5,53 @@
 
 #define CONDITION_NOT_MEET -1
 
-int is_empty_or_null(const char* numbers) {
-    return numbers == NULL || strlen(numbers) == 0;
+int ReturnZeroForEmptyOrNullOrZeroInput(const char* numbers) {
+    if (numbers == NULL || strlen(numbers) == 0 || strcmp(numbers, "0") == 0) {
+        return 0;
+    }
+    return CONDITION_NOT_MEET; 
 }
 
-int is_single_zero(const char* numbers) {
-    return strcmp(numbers, "0") == 0;
-}
-
-int ignore_numbers_greater_than_1000(int num) {
+int IgnoreNumbersGreaterThan1000(int num) {
     return num <= 1000 ? num : 0;
 }
 
-int sum_with_delimiter(const char* numbers, char delimiter) {
+char* ReplaceNewlineWithDelimiter(const char* numbers, char delimiter) {
+    char* modifiableNums = strdup(numbers);
+    for (char* p = modifiableNums; *p; ++p) {
+        if (*p == '\n') *p = delimiter;
+    }
+    return modifiableNums;
+}
+
+int SumWithDelimiter(const char* numbers, char delimiter) {
     int sum = 0;
-    char* modifiable_nums = strdup(numbers);
+    char* modifiableNums = ReplaceNewlineWithDelimiter(numbers, delimiter);
     char* token = strtok(modifiableNums, &delimiter);
     while (token != NULL) {
         int num = atoi(token);
-        sum += ignore_numbers_greater_than_1000(num);
+        sum += IgnoreNumbersGreaterThan1000(num);
         token = strtok(NULL, &delimiter);
     }
     free(modifiableNums);
     return sum;
 }
 
-int detect_custom_delimiter(const char* numbers) {
-    return strncmp(numbers, "//", 2) == 0 ? numbers[2] : CONDITION_NOT_MEET;
+int SumWithDefaultDelimiter(const char* numbers) {
+    return SumWithDelimiter(numbers, ',');
 }
 
-int detect_newline_delimiter(const char* numbers) {
-    return strchr(numbers, '\n') != NULL;
+int HandleSpecialCases(const char* numbers) {
+    return ReturnZeroForEmptyOrNullOrZeroInput(numbers);
 }
 
 int add(const char* numbers) {
-    int result;
+    int result = HandleSpecialCases(numbers);
+    if (result != CONDITION_NOT_MEET) return result;
 
-    if (is_empty_or_null(numbers)) {
-        return 0;
+    if (strncmp(numbers, "//", 2) == 0) {
+        return SumWithDelimiter(numbers + 4, numbers[2]);
     }
 
-    if (is_single_zero(numbers)) {
-        return 0;
-    }
-
-    if (detect_custom_delimiter(numbers)) {
-        return sum_with_delimiter(numbers + 4, detect_custom_delimiter(numbers));
-    }
-
-    if (detect_newline_delimiter(numbers)) {
-        return sum_with_delimiter(numbers, '\n');
-    }
-
-    return sum_with_delimiter(numbers, ',');
+    return SumWithDefaultDelimiter(numbers);
 }
