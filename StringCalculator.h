193971487503db1c9021c2b356b4807f0b44f6ad
RoopashreeHT <BHAL1KOR@bosch.com>
2024-09-13 18:07:05 +0530

    validating the first 3 test cases

--- StringCalculator.h
+++ StringCalculator.h
@@ -4,7 +4,23 @@
 
 int add(const char* numbers)
 {
-    return 0;
+    if (numbers == NULL || strlen(numbers) == 0) {
+        return 0;
+    }
+    if (strcmp(numbers, "0") == 0) {
+        return 0;
+    }
+
+    int sum = 0;
+    char* numbersCopy = strdup(numbers);
+    char* token = strtok(numbersCopy, ",");
+    while (token != NULL) {
+        sum += atoi(token);
+        token = strtok(NULL, ",");
+    }
+    free(numbersCopy);
+    return sum;
+
 }
 
 
